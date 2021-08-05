ec57f7257d3d4e81f21ea185b590715932014fc5
Abibubble <abi.yamaha.neo@gmail.com>
2021-08-05 22:00:10 +0000

    feat: Add auto-updating copyright functionality

--- static/js/script.js
+++ static/js/script.js
@@ -26,3 +26,13 @@ function animate(){
     requestAnimationFrame(animate);
 }
 animate();
+
+// -------------------------------------------------------------------- Copyright
+
+function copyrightYear() {
+    var d = new Date();
+    var y = d.getFullYear();
+    document.getElementById("copyright").innerHTML = y;
+}
+
+copyrightYear();
