433609119a9264edae215a29e984b4a5a8bfa8af
Alex <pioneerko@hotmail.com>
2021-08-05 22:10:25 +0100

    tux is walking now

--- static/js/script.js
+++ static/js/script.js
@@ -5,11 +5,12 @@ const CANVAS_HEIGHT = canvas.heigh = 600;
 
 const playerImage = new Image();
 playerImage.src = ('static/animations/penguin/penguin_walk.png');
-const spriteWidth = 69;
+const spriteWidth = 63.2;
 const spriteHeight = 64;
 let frameX = 0;
 let frameY = 0;
-
+let gameFrame = 0;
+const staggerFrames = 4;
 
 
 
@@ -17,8 +18,11 @@ function animate(){
     ctx.clearRect(0,0, canvas.width, canvas.heigh);
     // ctx.fillRect(100,50,100,100);
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0 ,0, spriteWidth, spriteHeight);
-    if (frameX < 4) frameX++;
-    else frameX = 0;
+   if (gameFrame % staggerFrames == 0){
+       if (frameX < 3) frameX++;
+       else frameX = 0;
+   }
+   gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
