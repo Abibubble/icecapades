0422f29fab2ca57e58d0fa9ee5a9e571fd0b0be7
Alex <pioneerko@hotmail.com>
2021-08-07 13:01:37 +0100

    Code:  slug js modified

--- static/js/slug.js
+++ static/js/slug.js
@@ -5,75 +5,45 @@ let lastTime = 0;
 
 class Slug {
     constructor() {
-        this.x = 250;
-        this.y = 0;
+        this.x = canvas.width+10;
+        this.y = canvas.height-floorHeight;
         this.vy = 0;
-        this.originalWidth = 64;
-        this.originalHeight = 70;
-        this.width = this.originalWidth;
-        this.height = this.originalHeight;
+        this.originalWidth = 258;
+        this.originalHeight = 170;
+        this.sizeModifier = Math.random() * 0.2 + 0.5;
+        this.width = this.originalWidth * this.sizeModifier;
+        this.height = this.originalHeight * this.sizeModifier;
         this.weight = 1;
         this.frameX = 0;
         this.jumping = true;
         this.sliding = false;
         this.charSlideWidth = 40;
         this.charSlideheight = 20;
-        this.directionX = Math.random() * 5 + 3;
+        this.directionX = Math.random() * 0.1 + 2;
+        this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = "static/animations/enemies/slug/slug-sprite1.png"; //894 x 178
     }
 
-    update() {
+    update(deltatime) {
         this.x -= this.directionX;
-        // //Stop character at floor level
-        // if (this.y > canvas.height - this.height) {
-        //     this.y = canvas.height - this.height;
-        //     this.vy = 0;
-        // } else {
-        //     //Otherwise set y velocity to act as gravity, along with setting y co-ord += y velocity
-        //     this.vy += this.weight;
-        //     this.vy *= 0.9; // slow down velocity
-        //     this.y += this.vy;
-        // }
-        // //Don't allow character out the top of canvas
-        // if (this.y < 0 + this.height) {
-        //     this.y = 0 + this.height;
-        //     this.vy = 0;
-        // }
-        // //If space bar pressed and char not jumping, call jump
-        // // if (spacePressed && !this.jumping) {
-        // //     this.jump();
-        // // }
-        // //Check if char is on floor, if so, set jumping to false;
-        // if (this.y > canvas.height - this.height) {
-        //     this.jumping = false;
-        // }
-        //If ArrowRight pressed
-        // if (arrowRightPressed && !this.sliding) {
-        //     this.slide();
-        // }
-        // Once let go of arrow down, set hitbox back to original position
-        // if (!arrowDownPressed) {
-        //     this.height = this.originalHeight;
-        //     this.width = this.originalWidth;
-        // }
+        if (this.x < 0 - this.width) this.markedForDeletion = true;
+
     }
     draw() {
         ctx.fillStyle = "red";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        if (this.y > canvas.height - this.height && !this.sliding) {
-            playerImage.src = "static/animations/enemies/slug/slug-sprite1.png"; //894 x 178
-        }
-        ctx.drawImage(
-            playerImage,
-            frameX * this.originalWidth,
-            0,
-            this.originalWidth,
-            this.originalHeight,
-            this.x,
-            this.y,
-            this.width + 5,
-            this.height+350,//this.height + 10,
-            this.originalWidth, //dwidth
-            this.originalHeight  //dheight
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage( //sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+            this.image,
+            0,//this.x, //sx
+            0,//this.y, //sy
+            this.originalWidth,//this.originalWidth, //sw
+            this.originalHeight,//this.originalHeight, //sh
+            this.x, //dx
+            this.y, //dy
+            this.width, //dw
+            this.height,//this.height + 10, dh
+
         );
     }
 }
@@ -91,8 +61,9 @@ function animate(timestamp) {
         timeToNextSlug = 0;
         console.log(slugs);
     }
-    [...slugs].forEach(object => object.update());
+    [...slugs].forEach(object => object.update(deltatime));
     [...slugs].forEach(object => object.draw());
+    slugs = slugs.filter(object => !object.markedForDeletion) //create new array without those marked for deletion
     requestAnimationFrame(animate);
 }
 
