a981ab1d48a3fe17cfb3817c11d894bbf8bcf461
Aidan Thorpe <aidant842@gmail.com>
2021-08-07 11:20:06 +0100

    added handle background

--- static/js/character-control.js
+++ static/js/character-control.js
@@ -2,14 +2,15 @@ const playerImage = new Image();
 playerImage.src = "static/animations/penguin/penguin_walk.png";
 
 function tuxControl() {
-  tux.update();
-  tux.draw();
-  if (gameFrame % staggerFrames == 0) {
-      if (frameX < 3) frameX++;
-      else frameX = 0;
-  }
-  requestAnimationFrame(tuxControl);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    handleBackground();
+    tux.update();
+    tux.draw();
+    if (gameFrame % staggerFrames == 0) {
+        if (frameX < 3) frameX++;
+        else frameX = 0;
+    }
+    requestAnimationFrame(tuxControl);
 }
 
 tuxControl();
-  
