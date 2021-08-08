95532af316452050bac520d9a4a79788616abc8c
Abibubble <abi.yamaha.neo@gmail.com>
2021-08-08 13:05:21 +0000

    feat: Add catching fish functionality

--- static/js/tux.js
+++ static/js/tux.js
@@ -164,6 +164,17 @@ class Tux {
             }
         }
 
+        for (let i = 0; i < fishArray.length; i++) { // See above
+            let fishX = fishArray[i].x;
+            let fishY = fishArray[i].y;
+            if ((fishX > this.x && fishX < this.x + this.width) || (fishX + fish.width > this.x && fishX + fish.width < this.x + this.width)) {
+                if ((fishY > this.y && fishY < this.y + this.height) || (fishY + fish.height > this.y && fishY + fish.height < this.y + this.height)) {
+                    fishArray.pop(fishArray[i]);
+                    tuxGetsAFish();
+                }
+            }
+        }
+
         if (!((currentEnemy / 2) >= this.x)) { // I want this without the !, but it breaks everything. No clue why!
             tuxBeingHurt = false; // But if currentEnemy x value is less than Tux's, Tux is no longer being hit
             currentEnemy = null;
